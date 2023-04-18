c5ef536b3246865813453b66bfd73658248e2e37
JHuelsebusch <jhuelsebusch@gmx.de>
2023-04-18 08:32:48 +0200

    added github link on object

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,7 +11,7 @@ export class PortfolioComponent {
     technologies: 'JavaScript | Rest-Api | HTML | CSS',
     description: 'Based on the PokéAPI a simple libary that provides and catalogues pokémon information',
     link: '',
-    github: '',
+    github: 'https://github.com/JHuelsebusch/pokedex',
     img: 'pokedex.png'
 
   }, {
@@ -19,14 +19,14 @@ export class PortfolioComponent {
     technologies: 'JavaScript | HTML | CSS',
     description: 'Task manager inspired by the Kanban System. Create and organize task using drag and drop functions, assign users and categories. This project was developed by group work.',
     link: '',
-    github: '',
+    github: 'https://github.com/JHuelsebusch/join',
     img: 'join.png'
   }, {
     name: 'Elves of Fate',
     technologies: 'JavaScript | HTML | SCSS',
     description: 'A simple Jump-and-Run game based on an object-oriented approach. Help the last remaining elf to find spellbooks and mana poisons to fight against the attacking orcs. ',
     link: '',
-    github: '',
+    github: 'https://github.com/JHuelsebusch/elves_of_fate',
     img: 'elves.png'
   }
   ]

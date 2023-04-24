1cb11dc130ce525e3bbd70cf0b2fbe6aeb9f07ff
JHuelsebusch <jhuelsebusch@gmx.de>
2023-04-24 13:32:31 +0200

    updated aos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit{
+  title = 'Portfolio | Janis Hülsebusch';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   scroll(id: HTMLElement) {
    id.scrollIntoView();
